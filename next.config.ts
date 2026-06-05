8719998dd65670abfab1bda6c38152919305d471
mahendra <mahendra@MacBook-Air-mahendra.local>
2026-06-05 14:40:06 +0700

    allow supabase storage images

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "auzlmqywzayjzurqythx.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
