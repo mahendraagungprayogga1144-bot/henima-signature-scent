923ff832889ed6667c308e5152c3cf2a8d52f750
Mahendra <mahendra@henimaofficial.com>
2026-06-12 11:05:47 +0700

    fix: remove double filter products

--- src/app/share-story/page.tsx
+++ src/app/share-story/page.tsx
@@ -13,7 +13,7 @@ export default function ShareStoryPage() {
 
   useEffect(()=>{
     fetch('/api/products-public').then(r=>r.json()).then((data:any)=>{
-      if(Array.isArray(data)) setProducts(data.filter((p:any)=>p.active).map((p:any)=>p.name))
+      if(Array.isArray(data)) setProducts(data.map((p:any)=>p.name))
     }).catch(()=>setProducts([]))
   },[])
 
