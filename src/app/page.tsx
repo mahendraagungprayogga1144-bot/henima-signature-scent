d219a00dfdc46b81c752101d351bd8d33321db7c
Mahendra <mahendra@henimaofficial.com>
2026-06-12 18:09:59 +0700

    fix: dynamic import LoveLettersSection to fix 500

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import ScrollReveal from "@/components/ScrollReveal";
-import LoveLettersSection from "@/components/LoveLettersSection";
+import dynamic from "next/dynamic";
+const LoveLettersSection = dynamic(() => import("@/components/LoveLettersSection"), { ssr: false });
 import AnnouncementBar from "@/components/AnnouncementBar";
 import PhotoCarousel from "@/components/PhotoCarousel";
 import HeroCarousel from "@/components/HeroCarousel";
